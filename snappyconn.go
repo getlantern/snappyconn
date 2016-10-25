d99ceb334e5503f023b658b83083aff6521d67ed
Ox Cart <ox.to.a.cart@gmail.com>
2016-10-25 06:02:30 -0500

    Simplified per code review

--- snappyconn.go
+++ snappyconn.go
@@ -3,17 +3,17 @@ package snappyconn
 import (
 	"github.com/golang/snappy"
 	"net"
-	"time"
 )
 
+// Wrap wraps a connection and adds snappy compression on reading and writing.
 func Wrap(wrapped net.Conn) net.Conn {
 	return &snappyconn{wrapped, snappy.NewReader(wrapped), snappy.NewBufferedWriter(wrapped)}
 }
 
 type snappyconn struct {
-	wrapped net.Conn
-	r       *snappy.Reader
-	w       *snappy.Writer
+	net.Conn
+	r *snappy.Reader
+	w *snappy.Writer
 }
 
 func (c *snappyconn) Read(b []byte) (n int, err error) {
@@ -26,28 +26,4 @@ func (c *snappyconn) Write(b []byte) (n int, err error) {
 		err = c.w.Flush()
 	}
 	return n, err
-}
-
-func (c *snappyconn) Close() error {
-	return c.wrapped.Close()
-}
-
-func (c *snappyconn) LocalAddr() net.Addr {
-	return c.wrapped.LocalAddr()
-}
-
-func (c *snappyconn) RemoteAddr() net.Addr {
-	return c.wrapped.RemoteAddr()
-}
-
-func (c *snappyconn) SetDeadline(t time.Time) error {
-	return c.wrapped.SetDeadline(t)
-}
-
-func (c *snappyconn) SetReadDeadline(t time.Time) error {
-	return c.wrapped.SetReadDeadline(t)
-}
-
-func (c *snappyconn) SetWriteDeadline(t time.Time) error {
-	return c.wrapped.SetWriteDeadline(t)
 }
